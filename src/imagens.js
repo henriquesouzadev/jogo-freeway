dfa2623fe0245264df9616aec48ba9e3bffc4eed
henriquesouzadev <henriquesouzadev@gmail.com>
2024-02-17 18:53:44 -0300

    ajuste imgs

--- src/imagens.js
+++ src/imagens.js
@@ -10,14 +10,14 @@ let soundCollision
 let soundPoint 
 
 function preload() {
-  imgRoad = loadImage('/assets/img/estrada.png')
-  imgActor = loadImage('/assets/img/ator-1.png')
-  imgCar1 = loadImage('/assets/img/carro-1.png')
-  imgCar2 = loadImage('/assets/img/carro-2.png')
-  imgCar3 = loadImage('/assets/img/carro-3.png')
+  imgRoad = loadImage('./assets/img/estrada.png')
+  imgActor = loadImage('./assets/img/ator-1.png')
+  imgCar1 = loadImage('./assets/img/carro-1.png')
+  imgCar2 = loadImage('./assets/img/carro-2.png')
+  imgCar3 = loadImage('./assets/img/carro-3.png')
   imgCars = [imgCar1, imgCar2, imgCar3, imgCar1, imgCar2, imgCar3]
 
-  soundTrack = loadSound('/assets/sounds/sons_trilha.mp3')
-  soundCollision = loadSound('/assets/sounds/sons_colidiu.mp3')
-  soundPoint = loadSound('/assets/sounds/pontos.wav')
+  soundTrack = loadSound('./assets/sounds/sons_trilha.mp3')
+  soundCollision = loadSound('./assets/sounds/sons_colidiu.mp3')
+  soundPoint = loadSound('./assets/sounds/pontos.wav')
 }
